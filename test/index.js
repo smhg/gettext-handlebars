565e2a27366d486bd08f00cfd4c54ebe42e23b1f
smhg <sam@strictlyphp.com>
2014-08-22 18:49:44 +0200

    add jshint to test

--- test/index.js
+++ test/index.js
@@ -3,32 +3,42 @@ var Parser = require('..'),
   assert = require('assert');
 
 describe('Parser', function () {
-  describe('#()', function () {
-    it('should have default keyword spec when none is passed', function () {
-      assert.ok((new Parser()).keywordSpec.gettext.length > 0);
-    });
+    describe('#()', function () {
+        it('should have default keyword spec when none is passed', function () {
+            assert((new Parser()).keywordSpec.gettext.length > 0);
+          });
+      });
+
+    describe('#parse()', function () {
+        it('should return results', function (done) {
+            fs.readFile(__dirname + '/fixtures/template.hbs', {encoding: 'utf8'}, function (err, data) {
+                if (err) {
+                  throw err;
+                }
+
+                var result = (new Parser()).parse(data);
+
+                assert.equal(typeof result, 'object');
+                assert.equal(Object.keys(result).length, 6);
+                assert.equal(result['Image description'].line.length, 2);
+
+                done();
+              });
+          });
+
+        it('should return plural results', function (done) {
+            fs.readFile(__dirname + '/fixtures/plural.hbs', {encoding: 'utf8'}, function (err, data) {
+                if (err) {
+                  throw err;
+                }
+
+                var result = (new Parser()).parse(data);
+
+                assert.equal(Object.keys(result).length, 2);
+                assert.equal(result['default'].plural, 'defaults');
+
+                done();
+              });
+          });
+      });
   });
-
-  describe('#parse()', function () {
-    var parser = new Parser(),
-      template,
-      result;
-
-    it('should return results', function () {
-      template = fs.readFileSync(__dirname + '/fixtures/template.hbs', 'utf8'),
-      result = parser.parse(template);
-
-      assert.equal(typeof result, 'object');
-      assert.equal(Object.keys(result).length, 6);
-      assert.equal(result['Image description'].line.length, 2);
-    });
-
-    it('should return plural results', function () {
-      template = fs.readFileSync(__dirname + '/fixtures/plural.hbs', 'utf8');
-      result = parser.parse(template);
-
-      assert.equal(Object.keys(result).length, 2);
-      assert.equal(result['default'].plural, 'defaults');
-    });
-  });
-});
